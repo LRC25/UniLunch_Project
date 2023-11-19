53fc6baee25e7b9776ad159d0c47b05a480bb160
UntetheredJ <101756457+UntetheredJ@users.noreply.github.com>
2023-11-19 17:36:45 -0500

    Fix plates container.

--- unilunch/lib/restaurants_page_widget.dart
+++ unilunch/lib/restaurants_page_widget.dart
@@ -73,118 +73,123 @@ class _RestaurantsPageWidgetState extends State<RestaurantsPageWidget> {
         body: SafeArea(
           top: true,
           child: Padding(
-  padding: EdgeInsetsDirectional.fromSTEB(10, 10, 10, 10),
-  child: Container(
-    width: MediaQuery.sizeOf(context).width,
-    height: MediaQuery.sizeOf(context).height * 1,
-    decoration: BoxDecoration(
-      color: FlutterFlowTheme.of(context).secondaryBackground,
-    ),
-    child: Column(
-      mainAxisSize: MainAxisSize.max,
-      children: [
-        Flexible(
-          child: Container(
-            width: MediaQuery.sizeOf(context).width,
-            height: 100,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                  blurRadius: 4,
-                  color: Color(0x33000000),
-                  offset: Offset(0, 2),
-                )
-              ],
-              borderRadius: BorderRadius.circular(15),
-              shape: BoxShape.rectangle,
-            ),
-            child: Padding(
-              padding: EdgeInsetsDirectional.fromSTEB(5, 5, 5, 5),
-              child: Row(
+            padding: EdgeInsetsDirectional.fromSTEB(10, 10, 10, 10),
+            child: Container(
+              width: MediaQuery.sizeOf(context).width,
+              height: MediaQuery.sizeOf(context).height * 1,
+              decoration: BoxDecoration(
+                color: FlutterFlowTheme.of(context).secondaryBackground,
+              ),
+              child: Column(
                 mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  Container(
-                    width: MediaQuery.sizeOf(context).width * 0.7,
-                    height: 100,
-                    decoration: BoxDecoration(
-                      color: FlutterFlowTheme.of(context).secondaryBackground,
-                    ),
-                    child: Padding(
-                      padding: EdgeInsetsDirectional.fromSTEB(0, 0, 5, 0),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.max,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Align(
-                            alignment: AlignmentDirectional(-1.00, 0.00),
-                            child: Text(
-                              'Milanesa de Pollo',
-                              style: FlutterFlowTheme.of(context)
-                                  .bodyMedium
-                                  .override(
-                                    fontFamily: 'Readex Pro',
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                            ),
-                          ),
-                          Text(
-                            'Deliciosa milanesa con pollo y papas y arroz y me lo chupa',
-                            textAlign: TextAlign.justify,
-                            style: FlutterFlowTheme.of(context).bodyMedium,
-                          ),
-                          Row(
-                            mainAxisSize: MainAxisSize.max,
-                            children: [
-                              Align(
-                                alignment: AlignmentDirectional(-1.00, 0.00),
-                                child: Text(
-                                  '\$15.000',
-                                  style: FlutterFlowTheme.of(context)
-                                      .bodyMedium
-                                      .override(
-                                        fontFamily: 'Readex Pro',
-                                        color: Color(0xFF138D20),
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.w300,
-                                      ),
-                                ),
-                              ),
-                              Flexible(
-                                child: Align(
-                                  alignment: AlignmentDirectional(1.00, 0.00),
-                                  child: Text(
-                                    '15 disponibles',
-                                    textAlign: TextAlign.start,
-                                    style: FlutterFlowTheme.of(context)
-                                        .bodyMedium
-                                        .override(
-                                          fontFamily: 'Readex Pro',
-                                          fontWeight: FontWeight.w300,
-                                        ),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Expanded(
+                  Flexible(
                     child: Container(
-                      width: MediaQuery.sizeOf(context).width * 0.3,
+                      width: MediaQuery.sizeOf(context).width,
                       height: 100,
-                      decoration: BoxDecoration(),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(8),
-                        child: Image.network(
-                          'https://picsum.photos/seed/203/600',
-                          width: MediaQuery.sizeOf(context).width * 0.108,
-                          height: 200,
-                          fit: BoxFit.cover,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            blurRadius: 4,
+                            color: Color(0x33000000),
+                            offset: Offset(0, 2),
+                          )
+                        ],
+                        borderRadius: BorderRadius.circular(15),
+                        shape: BoxShape.rectangle,
+                      ),
+                      child: Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(5, 5, 5, 5),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Container(
+                              width: MediaQuery.sizeOf(context).width * 0.7,
+                              height: 100,
+                              decoration: BoxDecoration(
+                                color: FlutterFlowTheme.of(context).secondaryBackground,
+                              ),
+                              child: Padding(
+                                padding: EdgeInsetsDirectional.fromSTEB(0, 0, 5, 0),
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.max,
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Align(
+                                      alignment: AlignmentDirectional(-1.00, 0.00),
+                                      child: Text(
+                                        'Milanesa de Pollo',
+                                        style: FlutterFlowTheme.of(context)
+                                            .bodyMedium
+                                            .override(
+                                              fontFamily: 'Readex Pro',
+                                              fontSize: 16,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                      ),
+                                    ),
+                                    Text(
+                                      'Deliciosa milanesa con pollo y papas y arroz y me lo chupa',
+                                      textAlign: TextAlign.justify,
+                                      style: FlutterFlowTheme.of(context).bodyMedium,
+                                    ),
+                                    Row(
+                                      mainAxisSize: MainAxisSize.max,
+                                      children: [
+                                        Align(
+                                          alignment: AlignmentDirectional(-1.00, 0.00),
+                                          child: Text(
+                                            '\$15.000',
+                                            style: FlutterFlowTheme.of(context)
+                                                .bodyMedium
+                                                .override(
+                                                  fontFamily: 'Readex Pro',
+                                                  color: Color(0xFF138D20),
+                                                  fontSize: 16,
+                                                  fontWeight: FontWeight.w300,
+                                                ),
+                                          ),
+                                        ),
+                                        Flexible(
+                                          child: Align(
+                                            alignment: AlignmentDirectional(1.00, 0.00),
+                                            child: Text(
+                                              '15 disponibles',
+                                              textAlign: TextAlign.start,
+                                              style: FlutterFlowTheme.of(context)
+                                                  .bodyMedium
+                                                  .override(
+                                                    fontFamily: 'Readex Pro',
+                                                    fontWeight: FontWeight.w300,
+                                                  ),
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: Container(
+                                width: MediaQuery.sizeOf(context).width * 0.3,
+                                height: 100,
+                                decoration: BoxDecoration(),
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(8),
+                                  child: Image.network(
+                                    'https://picsum.photos/seed/203/600',
+                                    width: MediaQuery.sizeOf(context).width * 0.108,
+                                    height: 200,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     ),
@@ -192,15 +197,7 @@ class _RestaurantsPageWidgetState extends State<RestaurantsPageWidget> {
                 ],
               ),
             ),
-          ),
-        ),
-      ],
-    ),
-  ),
-)
-
-
-
+          )
         ),
       ),
     );
