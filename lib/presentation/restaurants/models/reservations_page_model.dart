317809f579461c479cb04add2a8c4fe7f12b68a2
UntetheredJ <101756457+UntetheredJ@users.noreply.github.com>
2023-12-03 10:20:41 -0500

    Fixed button animation for plates.

--- lib/presentation/restaurants/models/reservations_page_model.dart
+++ lib/presentation/restaurants/models/reservations_page_model.dart
@@ -30,106 +30,114 @@ class RestaurantReservationsPageModel
 
     return Padding(
       padding: EdgeInsetsDirectional.fromSTEB(0, 5, 0, 5),
-      child: InkWell(
-        splashColor: Colors.transparent,
-        focusColor: Colors.transparent,
-        hoverColor: Colors.transparent,
-        highlightColor: Colors.transparent,
-        onTap: () async {
-          detallesReservaPendiente(context, reserva);
-        },
-        child: Container(
-          width: MediaQuery.sizeOf(context).width,
-          height: 80,
-          decoration: BoxDecoration(
-            color: FlutterFlowTheme.of(context).secondaryBackground,
-            boxShadow: [
-              BoxShadow(
-                blurRadius: 4,
-                color: Color(0x33000000),
-                offset: Offset(0, 2),
-              )
-            ],
-            borderRadius: BorderRadius.circular(15),
-            shape: BoxShape.rectangle,
-          ),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Align(
-                alignment: AlignmentDirectional(0.00, 0.00),
-                child: Padding(
-                  padding: EdgeInsetsDirectional.fromSTEB(15, 0, 15, 0),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.account_circle,
-                        color: Color(0xFF064244),
-                        size: 32,
+      child: Container(
+        width: MediaQuery.sizeOf(context).width,
+        height: 80,
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 4,
+              color: Color(0x33000000),
+              offset: Offset(0, 2),
+            )
+          ],
+          borderRadius: BorderRadius.circular(15),
+          shape: BoxShape.rectangle,
+        ),
+        child: Material(
+          borderRadius: BorderRadius.circular(15),
+          color: FlutterFlowTheme.of(context).secondaryBackground,
+          child: InkWell(
+            // splashColor: Colors.transparent,
+            // focusColor: Colors.transparent,
+            // hoverColor: Colors.transparent,
+            // highlightColor: Colors.transparent,
+            onTap: () async {
+              detallesReservaPendiente(context, reserva);
+            },
+            child: Container(
+              width: MediaQuery.sizeOf(context).width,
+              height: 80,
+              color: Colors.transparent,
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Align(
+                    alignment: AlignmentDirectional(0.00, 0.00),
+                    child: Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(15, 0, 15, 0),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.account_circle,
+                            color: Color(0xFF064244),
+                            size: 32,
+                          ),
+                          Icon(
+                            Icons.access_time,
+                            color: Color(0xFF064244),
+                            size: 32,
+                          ),
+                        ].divide(SizedBox(height: 5)),
                       ),
-                      Icon(
-                        Icons.access_time,
-                        color: Color(0xFF064244),
-                        size: 32,
-                      ),
-                    ].divide(SizedBox(height: 5)),
+                    ),
                   ),
-                ),
-              ),
-              Flexible(
-                child: Align(
-                  alignment: AlignmentDirectional(0.00, 0.00),
-                  child: Padding(
-                    padding: EdgeInsetsDirectional.fromSTEB(20, 0, 20, 0),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.max,
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          reserva.nombreUsuario,
-                          style: FlutterFlowTheme.of(context).bodyMedium.override(
-                                fontFamily: 'Readex Pro',
-                                color: Color(0xFF064244),
-                                fontSize: 16,
-                                fontWeight: FontWeight.w600,
-                              ),
-                        ),
-                        Row(
+                  Flexible(
+                    child: Align(
+                      alignment: AlignmentDirectional(0.00, 0.00),
+                      child: Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(20, 0, 20, 0),
+                        child: Column(
                           mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             Text(
-                              '$cantidad platos - ',
-                              style: FlutterFlowTheme.of(context)
-                                  .bodyMedium
-                                  .override(
+                              reserva.nombreUsuario,
+                              style: FlutterFlowTheme.of(context).bodyMedium.override(
+                                    fontFamily: 'Readex Pro',
+                                    color: Color(0xFF064244),
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                            ),
+                            Row(
+                              mainAxisSize: MainAxisSize.max,
+                              children: [
+                                Text(
+                                  '$cantidad platos - ',
+                                  style: FlutterFlowTheme.of(context)
+                                      .bodyMedium
+                                      .override(
+                                        fontFamily: 'Readex Pro',
+                                        color: Color(0xFF064244),
+                                        fontSize: 15,
+                                      ),
+                                ),
+                                estodoReserva(context, reserva)
+                              ],
+                            ),
+                            Text(
+                              DateFormat('yyyy-MM-dd HH:mm').format(reserva.fecha),
+                              style: FlutterFlowTheme.of(context).bodyMedium.override(
                                     fontFamily: 'Readex Pro',
                                     color: Color(0xFF064244),
                                     fontSize: 15,
+                                    fontWeight: FontWeight.w500,
+                                    fontStyle: FontStyle.italic,
                                   ),
                             ),
-                            estodoReserva(context, reserva)
                           ],
                         ),
-                        Text(
-                          DateFormat('yyyy-MM-dd HH:mm').format(reserva.fecha),
-                          style: FlutterFlowTheme.of(context).bodyMedium.override(
-                                fontFamily: 'Readex Pro',
-                                color: Color(0xFF064244),
-                                fontSize: 15,
-                                fontWeight: FontWeight.w500,
-                                fontStyle: FontStyle.italic,
-                              ),
-                        ),
-                      ],
+                      ),
                     ),
                   ),
-                ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
